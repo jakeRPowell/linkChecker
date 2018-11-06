6ec2fc7cb574d655cc764f4f64ada5d81317aa39
jakeRPowell <43782301+jakeRPowell@users.noreply.github.com>
2018-11-06 20:00:17 +0000

    Update linkChecker.php

--- linkChecker.php
+++ linkChecker.php
@@ -1,46 +1,92 @@
 <html>
  <head>
   <title>Link Checker</title>
- </head>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+
+.container {
+        max-width: 500px;
+    margin: auto;
+}
+
+</style>
+  
+</head>
  <body>
 
-<?php
-
-$url = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $url = test_input($_POST["url"]);
-  }
-
-function test_input($data) {
-  return $data;
- $html = file_get_contents($url);
-
-$doc = new DOMDocument();
-$doc->loadHTML($html);
-
-$xpath = new DOMXpath($doc);
-
-$nodes = $xpath->query('//a');
-
-foreach($nodes as $node) {
-    var_dump($node->getAttribute('href'));
-}
-}
 
 
-
-?>
-
-<h2>PHP Form Validation Example</h2>
+<h2>URL search</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  URL: <input type="text" name="url">
-  <input type="submit" name="submit" value="Submit">  
+  <input type="text" name="urlInput">
+  <input type="submit" name="submit" value="Search!">  
 </form>
 
 <?php
-echo "<h2>Your Input:</h2>";
-echo $url;
+
+$website = $_POST['urlInput'];
+
+$html = file_get_contents($website);
+
+$website = preg_replace('{/$}', '', $website);
+
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
+
+$xpath = new DOMXPath($dom);
+$hrefs = $xpath->evaluate("/html/body//a");
+
+for ($i = 0; $i < $hrefs->length; $i++) {
+        $href = $hrefs->item($i);
+        $url = $href->getAttribute('href');
+        $url = ltrim($url, '/');
+        $url = isAbsoluteUrl($url) ? $url : $website.'/'.$url;
+        get_http_response_code($url);
+}
+
+
+
+function get_http_response_code($url) {
+
+        $handle = curl_init($url);
+        curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+
+        /* Get the HTML or whatever is linked in $url. */
+        $response = curl_exec($handle);
+
+        /* Check for 404 (file not found). */
+        $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        if($httpCode == 200) {
+                echo $url . ": <span style='color: green'>ok => $httpCode</span> <br>";
+        } else if($httpCode == 301 || $httpCode == 302) {
+                echo $url . ": <span style='color: orange'>Redirected => $httpCode</span> <br>";
+        } else {
+                echo $url . ": <span style='color: red'>Not working => $httpCode</span> <br>";
+        }
+
+
+
+        curl_close($handle);
+}
+
+function isAbsoluteUrl($url)
+    {
+        if (strpos($url, 'http://') !== false || strpos($url, 'https://') !== false) {
+                return true;
+              }
+              
+              return false;
+    }
+
 ?>
+
+<?php
+echo $result;
+?>
+
+
+
+
  </body>
 </html>
